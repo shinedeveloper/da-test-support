f31b28825bc1bb0205ae3e36a8af3859076b86b4
Kamil Ševeček <kamil@sevecek.net>
2021-04-24 09:36:45 +0200

    Simple-Selenium-app

---
+++ Selenium-1/src/main/java/com/example/selenium/Bootstrap.java
@@ -0,0 +1,9 @@
+package com.example.selenium;
+
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        var program = new HlavniProgram();
+        program.run();
+    }
+}
---
+++ Selenium-1/src/main/java/com/example/selenium/HlavniProgram.java
@@ -0,0 +1,49 @@
+package com.example.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class HlavniProgram {
+
+    private WebDriver browser;
+
+    public void run() {
+        System.setProperty("webdriver.gecko.driver", "C:\\Java-Training\\Selenium\\geckodriver.exe");
+        browser = new FirefoxDriver();
+        try {
+
+            browser.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            browser.navigate().to("https://www.wikipedia.org/");
+
+            WebElement searchElement = browser.findElement(By.id("searchInput"));
+            searchElement.sendKeys("Hello World program");
+            searchElement.submit();
+
+            waitUntilJavaScriptFindsElement("#History");
+            WebElement historyParagraph = browser.findElement(By.cssSelector("#History"));
+            System.out.println("Success");
+
+        } finally {
+            browser.close();
+        }
+    }
+
+    private void waitUntilJavaScriptFindsElement(String cssSelector) {
+        waitUntilJavaScriptReturnsNonNull("return document.querySelector('"+cssSelector+"');");
+    }
+
+    private void waitUntilJavaScriptReturnsNonNull(String script) {
+        WebDriverWait wait = new WebDriverWait(browser, 60);
+        wait.until(it -> {
+            JavascriptExecutor javascriptExecutor = (JavascriptExecutor) it;
+            Object result = javascriptExecutor.executeScript(script);
+            return result != null;
+        });
+    }
+}
