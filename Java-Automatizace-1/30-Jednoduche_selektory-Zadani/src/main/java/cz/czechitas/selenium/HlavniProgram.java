a3b841548e11ec9b25da1370cd6e612aecded974
Kamil Ševeček <kamil@sevecek.net>
2021-04-29 20:46:13 +0200

    Update4

--- Java-Automatizace-1/30-Jednoduche_selektory-Zadani/src/main/java/cz/czechitas/selenium/HlavniProgram.java
+++ Java-Automatizace-1/30-Jednoduche_selektory-Zadani/src/main/java/cz/czechitas/selenium/HlavniProgram.java
@@ -3,7 +3,6 @@ package cz.czechitas.selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -14,26 +13,51 @@ public class HlavniProgram {
 //      System.setProperty("webdriver.gecko.driver", System.getProperty("user.home") + "/Java-Training/Selenium/geckodriver");
         System.setProperty("webdriver.gecko.driver", "C:\\Java-Training\\Selenium\\geckodriver.exe");
 
-        WebDriver prohlizec = new FirefoxDriver();
+        WebDriver prohlizec;
+        prohlizec = new FirefoxDriver();
+        try {
 
-        prohlizec.navigate().to("https://automation.shinekamil.repl.co/");
+            prohlizec.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            prohlizec.navigate().to("https://automation.shinekamil.repl.co/");
 
-        Thread.sleep(5_000);
+            WebElement zalozkaButtons = prohlizec.findElement(By.id("selectorsButtons"));
+            zalozkaButtons.click();
 
-        WebElement zalozkaButtons = prohlizec.findElement(By.xpath("//li[@id='selectorsButtons']/a"));
-//        WebElement zalozkaButtons = prohlizec.findElement(By.xpath("//ul/li"));
-        zalozkaButtons.click();
+            WebElement idButton = prohlizec.findElement(By.id("button1"));
+            idButton.click();
+            WebElement nameButton = prohlizec.findElement(By.name("clickingOnMe"));
+            nameButton.click();
+            WebElement classNameButton = prohlizec.findElement(By.className("click-click-click"));
+            classNameButton.click();
+            WebElement theButton = prohlizec.findElement(By.xpath("//button[text() = 'This is THE button']"));
+            theButton.click();
+            WebElement partialButton = prohlizec.findElement(By.partialLinkText("Partial"));
+//            WebElement partialButton = prohlizec.findElement(By.xpath("//button[contains(text(), 'Partial')]"));
+            partialButton.click();
+            WebElement dataButton = prohlizec.findElement(By.xpath("//button[@data-qa='customAttribute-button']"));
+            dataButton.click();
+            WebElement xpathButton = prohlizec.findElement(By.xpath("//p/button"));
+            xpathButton.click();
+            WebElement mimoButton = prohlizec.findElement(By.xpath("/html/body/div/button"));
+            mimoButton.click();
 
-        Thread.sleep(5_000);
+            Thread.sleep(5_000);
 
-        WebElement tlacitkoButton1 = prohlizec.findElement(By.id("button1"));
-        tlacitkoButton1.click();
+            WebElement zalozkaLogin = prohlizec.findElement(By.id("login"));
+            zalozkaLogin.click();
 
-        Thread.sleep(10_000);
+            WebElement polickoUsername = prohlizec.findElement(By.id("login-username"));
+            polickoUsername.sendKeys("czechitas");
+            WebElement polickoPassword = prohlizec.findElement(By.id("login-password"));
+            polickoPassword.sendKeys("D4Ostr4v42020");
+            WebElement loginButton = prohlizec.findElement(By.id("login-submit"));
+            loginButton.click();
 
-        prohlizec.close();
+            Thread.sleep(10_000);
 
-        // Prestavka do 18:50
+        } finally {
+            prohlizec.close();
+        }
     }
 
 }
