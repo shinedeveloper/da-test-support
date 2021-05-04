0399a0e9a9f91dd857cfe34e934ef28ca40d0401
Kamil Ševeček <kamil@sevecek.net>
2021-05-04 19:19:51 +0200

    Update1

--- Java-Automatizace-2/10-Pokrocile_Selenium-Zadani/src/main/java/cz/czechitas/selenium/HlavniProgram.java
+++ Java-Automatizace-2/10-Pokrocile_Selenium-Zadani/src/main/java/cz/czechitas/selenium/HlavniProgram.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class HlavniProgram {
@@ -16,9 +17,14 @@ public class HlavniProgram {
 
         WebDriver prohlizec;
         prohlizec = new FirefoxDriver();
-        prohlizec.navigate().to("https://automation2.shinekamil.repl.co/");
+        prohlizec.navigate().to("https://automation2.shinekamil.repl.co/selectors.html");
 
-        // TODO: Sem vepiste svuj program
+        List<WebElement> seznamOdstavcu = prohlizec.findElements(By.xpath("//*[@id='pohadka']/p"));
+        for (int i=0; i<seznamOdstavcu.size(); i++) {
+            WebElement jedenOdstavec = seznamOdstavcu.get(i);
+            System.out.println("Odstavec: "+i);
+            System.out.println(jedenOdstavec.getText());
+        }
 
         Thread.sleep(10_000);
         prohlizec.close();
